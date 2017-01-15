c01fcd717d72bd417596d74bf48adbdf0a1c1334
Korobko Alex <korobkoalexander5901@gmail.com>
2017-01-16 01:53:03 +0200

    added new use case

---
+++ src/main/java/com/example/ServerVerticleWithConfiguration.java
@@ -0,0 +1,28 @@
+package com.example;
+
+import com.github.invizible.Verticle;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.json.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+@Verticle(deploymentOptionsBeanName = "options1")
+public class ServerVerticleWithConfiguration extends AbstractVerticle {
+
+  @Autowired
+  private TestService testService;
+
+  @Bean
+  public DeploymentOptions options1() {
+    return new DeploymentOptions().setConfig(new JsonObject().put("author", "Invizible"));
+  }
+
+  @Override
+  public void start() throws Exception {
+    HttpServer httpServer = vertx.createHttpServer()
+      .requestHandler(request -> request.response().end(config().getString("author")))
+      .listen(8081);
+  }
+}
