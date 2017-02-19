c34379cb2cc06d7a2cf8de71fb1115c89217930b
Korobko Alex <korobkoalexander5901@gmail.com>
2017-02-19 21:56:57 +0200

    Added Component scan filter

--- src/main/java/com/github/invizible/vertx/springaware/examples/clustered/SpringVertxApplication.java
+++ src/main/java/com/github/invizible/vertx/springaware/examples/clustered/SpringVertxApplication.java
@@ -5,10 +5,15 @@ import io.vertx.core.DeploymentOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 
 @Import(ClusteredVertxConfiguration.class) // 1. Import Vertx configuration, so clustered Vert.x instance will be available
 @SpringBootApplication
+@ComponentScan(excludeFilters = {
+  @ComponentScan.Filter(type = FilterType.REGEX, pattern = {"com.github.invizible.vertx.springaware.examples.clustered.eventbus.*"})
+})
 public class SpringVertxApplication {
 
   public static void main(String[] args) {
